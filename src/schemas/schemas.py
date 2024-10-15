75cea9342bcdca51433387bd14bfb58040307ac2
Maniakalochka <maniakalochka@gmail.com.com>
2024-10-15 12:06:33 +0300

    Add pydantic schemas for Product and Category

---
+++ src/schemas/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class CreateProduct(BaseModel):
+    name: str
+    description: str
+    price: float
+    image_url: str
+    stock: int
+    category: int
+
+
+class CreateCategory(BaseModel):
+    name: str
+    parent_id: int | None
