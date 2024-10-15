49eaa219eb83cac5470fbd1ff001398973adf7b7
Maniakalochka <maniakalochka@gmail.com.com>
2024-10-15 10:59:32 +0300

    include router for category

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, APIRouter
+from routers import category
 
 app = FastAPI()
+
+@app.get("/") 
+async def weclome() -> dict:
+    return {"message": "Welcome to FastAPI"}
+
+app.include_router(category.router)
 
 if __name__ == "__main__":
     import uvicorn
