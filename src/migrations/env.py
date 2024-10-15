c2579c9b32bc69a4e39b2e01bce93ccdb0182753
Maniakalochka <maniakalochka@gmail.com.com>
2024-10-15 20:36:08 +0300

    Fix module import in migrations/env.py

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,4 +1,7 @@
 from logging.config import fileConfig
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from src.backend.db import Base
 from src.models.category import Category
