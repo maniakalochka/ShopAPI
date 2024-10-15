d09e9701851aa3bb784e838fd7024df4f13299f6
Maniakalochka <maniakalochka@gmail.com.com>
2024-10-15 20:37:15 +0300

    add endpoint for post query /create.

--- src/routers/category.py
+++ src/routers/category.py
@@ -1,4 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from src.backend.db_depends import get_db
+from typing import Annotated
+
+from src.models import *
+from sqlalchemy import insert  
+from src.schemas.schemas import CreateCategory
+
+from slugify import slugify
+
 
 router = APIRouter(prefix='/category', tags=['category'])
 
@@ -9,8 +19,17 @@ async def get_all_categories():
 
 
 @router.post('/create')
-async def create_category():
-    pass
+async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory):
+    db.execute(insert(Category).values(
+        name=create_category.name,
+        slug=slugify(create_category.name),
+        parent_id=create_category.parent_id))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
+
 
 
 @router.put('/update_category')
